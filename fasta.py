5af71a44c5a8c2af12f6a4707a9d21681d4664a5
michael <ttmigueltt@gmail.com>
2016-06-20 10:40:30 +1000

    Task 2

--- fasta.py
+++ fasta.py
@@ -8,10 +8,20 @@ class FastaParser:
 
     # Returns a string that will be the program's output
     def parse(self):
-        i = 0
+        seq_count = 0
+        seq_min = float('inf')
+        seq_max = float('-inf')
+
         for seq in SeqIO.parse(self.filename, "fasta"):
-            i += 1
-        return str(i)
+            length = len(seq)
+
+            seq_count += 1
+            if length < seq_min:
+                seq_min = length
+            if length > seq_max:
+                seq_max = length
+
+        return "{}  {}  {}".format(seq_count, seq_min, seq_max)
 
 
 if __name__ == '__main__':
